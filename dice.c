9198c62d03c6396aec01a34241c38e6696a1913f
bfentor <fentorb24@gmail.com>
2024-12-16 01:49:46 +0200

    Added wrapping to dice drawing

--- dice.c
+++ dice.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <time.h>
 #include <sys/time.h>
 #include <unistd.h> 
+
 #include <string.h>
+
+#include <sys/ioctl.h>
 
 int *mkList(int list[], int num) {
 
@@ -26,6 +30,13 @@ void draw(int num, char dice [][5][10], int list[]) {
     }
 }
 
+int getNumDice (void)
+{
+    struct winsize w;
+    ioctl(0, TIOCGWINSZ, &w);
+    return (w.ws_col / 11);
+}
+
 int main(int argc, char **argv) {
 
     int num = 1;
@@ -33,43 +44,52 @@ int main(int argc, char **argv) {
     	num = atoi(argv[1]);
             
 	// List of generated random numbers
-	int randList [num];
-        char dice [6][5][10];
-        strcpy(dice[0][0], "+-------+");
-        strcpy(dice[0][1], "|       |");
-        strcpy(dice[0][2], "|   O   |");
-        strcpy(dice[0][3], "|       |");
-        strcpy(dice[0][4], "+-------+");
-        
-        strcpy(dice[1][0], "+-------+");
-        strcpy(dice[1][1], "| O     |");
-        strcpy(dice[1][2], "|       |");
-        strcpy(dice[1][3], "|     O |");
-        strcpy(dice[1][4], "+-------+");
+    char dice [6][5][10];
+    strcpy(dice[0][0], "+-------+");
+    strcpy(dice[0][1], "|       |");
+    strcpy(dice[0][2], "|   O   |");
+    strcpy(dice[0][3], "|       |");
+    strcpy(dice[0][4], "+-------+");
+    
+    strcpy(dice[1][0], "+-------+");
+    strcpy(dice[1][1], "| O     |");
+    strcpy(dice[1][2], "|       |");
+    strcpy(dice[1][3], "|     O |");
+    strcpy(dice[1][4], "+-------+");
 
-        strcpy(dice[2][0], "+-------+");
-        strcpy(dice[2][1], "| O     |");
-        strcpy(dice[2][2], "|   O   |");
-        strcpy(dice[2][3], "|     O |");
-        strcpy(dice[2][4], "+-------+");
+    strcpy(dice[2][0], "+-------+");
+    strcpy(dice[2][1], "| O     |");
+    strcpy(dice[2][2], "|   O   |");
+    strcpy(dice[2][3], "|     O |");
+    strcpy(dice[2][4], "+-------+");
 
-        strcpy(dice[3][0], "+-------+");
-        strcpy(dice[3][1], "| O   O |");
-        strcpy(dice[3][2], "|       |");
-        strcpy(dice[3][3], "| O   O |");
-        strcpy(dice[3][4], "+-------+");
+    strcpy(dice[3][0], "+-------+");
+    strcpy(dice[3][1], "| O   O |");
+    strcpy(dice[3][2], "|       |");
+    strcpy(dice[3][3], "| O   O |");
+    strcpy(dice[3][4], "+-------+");
 
-        strcpy(dice[4][0], "+-------+");
-        strcpy(dice[4][1], "| O   O |");
-        strcpy(dice[4][2], "|   O   |");
-        strcpy(dice[4][3], "| O   O |");
-        strcpy(dice[4][4], "+-------+");
+    strcpy(dice[4][0], "+-------+");
+    strcpy(dice[4][1], "| O   O |");
+    strcpy(dice[4][2], "|   O   |");
+    strcpy(dice[4][3], "| O   O |");
+    strcpy(dice[4][4], "+-------+");
 
-        strcpy(dice[5][0], "+-------+");
-        strcpy(dice[5][1], "| O   O |");
-        strcpy(dice[5][2], "| O   O |");
-        strcpy(dice[5][3], "| O   O |");
-        strcpy(dice[5][4], "+-------+");
+    strcpy(dice[5][0], "+-------+");
+    strcpy(dice[5][1], "| O   O |");
+    strcpy(dice[5][2], "| O   O |");
+    strcpy(dice[5][3], "| O   O |");
+    strcpy(dice[5][4], "+-------+");
 
-        draw(num, dice, mkList(randList, num));
+        //calculate number of dice per line and number of lines
+        int dicePerLine = getNumDice();
+        do
+        {
+            if (num < dicePerLine)
+                dicePerLine = num;
+            int randList [dicePerLine];
+            draw(dicePerLine, dice, mkList(randList, dicePerLine));
+            num -= dicePerLine;
+            printf("\n");
+        } while (num > 0);
     }
